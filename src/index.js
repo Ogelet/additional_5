123b765f242fcda4a58c9479bc5a257ec992989f
Svetlana Telego <camarezi@gmail.com>
2018-03-01 13:06:22 +0300

    solution 2

--- src/index.js
+++ src/index.js
@@ -1,38 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
   let result = false;
-  const open = bracketsConfig[0][0];
-  const close = bracketsConfig[0][1];
-  let brackets = str.split('');
   let configLength = bracketsConfig.length;
-  function checkBrackets(){
-    if(brackets.length === 0){
-      console.log('result!!!!!!!!', result);
-      return result = true;
-    };
-    console.log('bracketsLength', brackets.length, 'result', result);
-    const indexCloseBracket = brackets.indexOf(close);
-    const indexOpenBracket = brackets.indexOf(open);
-    if ((indexCloseBracket === -1 && indexOpenBracket > -1) || (indexCloseBracket > -1 && indexOpenBracket === -1) ){
-      console.log('indexCloseBracket === -1 || indexOpenBracket === -1');
-      return result = false;
-    }
-    if(indexCloseBracket > 0){
-      for(let i =  indexCloseBracket - 1; i > -1; ){
-        console.log('brackets', brackets, 'цикл пошел','i', i,'indexCloseBracket', indexCloseBracket, 'indexOpenBracket', indexOpenBracket);
-        if(brackets[i] === open){
-          console.log('brackets', brackets, 'зашли в цикл','i', i,'indexCloseBracket', indexCloseBracket, 'indexOpenBracket', indexOpenBracket)
-          brackets.splice(indexCloseBracket,1);
-          brackets.splice(i,1);    
-          console.log('brackets.length', brackets.length, 'brackets', brackets, 'конец цикла','i', i,'indexCloseBracket', indexCloseBracket, 'indexOpenBracket', indexOpenBracket)    
-          checkBrackets();
-        } else 
-        if( i = 0){
-          return result = false
-        } else { i--; }
-    }} else { return result = false; };
+  for(let i = 0; i < configLength ; i++){
+    let arrayInString = bracketsConfig[i].join('');
+    bracketsConfig[i] = arrayInString;
   };
+  function lookAndDelete(substr){
+    index = str.indexOf(substr);
+    str = str.slice(0, index) + str.slice(index + 2, str.length)
+  }
+  function checkBrackets(){
+    let count = 0;
+    for(let i = 0; i < configLength ; i++){
+      if(str.indexOf(bracketsConfig[i]) > -1){
+          count++;
+      }
+    };
+    if (count > 0){
+      for(let i = 0; i < configLength ; i++){
+        if(str.indexOf(bracketsConfig[i]) > -1){
+            lookAndDelete(bracketsConfig[i]);
+        }
+      };
+      checkBrackets();
+    }
+    else if(str.length > 0){return result = false }
+    else {return result = true}
+  }
   checkBrackets();
-  console.log( result);
-  console.log("!!!!!!!!!!!!!!!!!!!!!!", result);
   return result;
-}; 
+}
